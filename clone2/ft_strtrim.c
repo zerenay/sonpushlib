ca642be3325d9fae6ed05c43d96ca30e290fd0f2
Zeren Aydoğan <161078250+zerenay@users.noreply.github.com>
2025-07-11 01:38:06 +0300

    Update ft_strtrim.c

--- clone2/ft_strtrim.c
+++ clone2/ft_strtrim.c
@@ -13,30 +13,19 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static int	is_in_set(char c, const char *set)
-{
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	end;
 	char	*trimmed;
 
-	start = 0;
 	if (!s1 || !set)
 		return (NULL);
+	start = 0;
 	end = ft_strlen(s1);
-	while (s1[start] && is_in_set(s1[start], set))
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	while (end > start && is_in_set(s1[end - 1], set))
+	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
 	trimmed = (char *)malloc(sizeof(char) * (end - start + 1));
 	if (!trimmed)
